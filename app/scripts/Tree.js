98c4fc83abd89498fcafe0a638ce6f3bcf417252
Wes Hardee <whardee@nerdery.com>
2015-03-18 13:36:51 -0500

    move tree drawing code to tree module

--- app/scripts/Tree.js
+++ app/scripts/Tree.js
@@ -1,3 +1,38 @@
-export function Tree() {
-    
+const NUM_TREE_RUNGS = 3;
+
+let canvas = document.getElementById('canvas');
+let c = canvas.getContext('2d');
+
+export class Tree {
+    constructor(x, height, color) {
+        this.x = x;
+        this.height = height;
+        this.color = color;
+
+        // compute additional properties
+        this.width = this.height / 4;
+    }
+
+    draw() {
+        let startY = canvas.height;
+        let rungWidth;
+        let rungBaseY;
+        let progress;
+
+        c.fillStyle = this.color;
+
+        // // draw first rung
+        for (var i = 0; i < NUM_TREE_RUNGS; i++) {
+            progress = i / NUM_TREE_RUNGS;
+
+            rungBaseY = -this.height * Math.pow(progress, 1.2) + startY;
+            rungWidth = -this.width * Math.pow(progress, 3) + this.width;
+
+            c.beginPath();
+            c.moveTo(this.x - rungWidth, rungBaseY);
+            c.lineTo(this.x, startY - this.height);
+            c.lineTo(this.x + rungWidth, rungBaseY);
+            c.fill();
+        }
+    }
 }
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,6 @@
 'use strict';
 
-import './Tree';
-
-// colors
-const TREE_COLOR = 'green';
-const FENCE_COLOR = 'brown';
+import {Tree} from './Tree';
 
 // initial setup
 var canvas = document.getElementById('canvas');
@@ -16,110 +12,92 @@ var centerY;
 
 export class App {
     constructor() {
-        resize();
-        window.onresize = resize;
+        this._trees = [];
+
+        this.resize();
+        this.addTrees();
+        this.draw();
+
+        window.onresize = () => {
+            this.resize();
+            this.draw();
+        };
+    }
+
+    addTrees() {
+        var i;
+        var x;
+        var color;
+        var height;
+        var progress;
+        var numTrees = 40;
+        var maxTreeHeight = 400;
+        var minTreeHeight = 220;
+        var treeHeightDelta = maxTreeHeight - minTreeHeight;
+
+        var h = 0;
+        var s = 0;
+        var l = 0;
+
+        var maxH = 150;
+        var minH = 100;
+        var deltaH = maxH - minH;
+
+        var maxS = 100;
+        var minS = 70;
+        var deltaS = maxS - minS;
+
+        var maxL = 40;
+        var minL = 25;
+        var deltaL = maxL - minL;
+
+        for (i = 0; i < numTrees; i++) {
+            // drawSky(1 / numTrees); // create atmosphere effect;
+
+            progress = i / numTrees;
+
+            h = Math.floor(deltaH * Math.random() + minH);
+            s = Math.floor(deltaS * Math.random() + minS);
+            l = Math.floor(deltaL * Math.random() + minL);
+
+            l = minL + Math.random() * progress * deltaL;
+
+            color = 'hsl(' + h + ', ' + s + '%, ' + l + '%)';
+
+            x = canvas.width * Math.random();
+            height = treeHeightDelta * Math.random() * progress + minTreeHeight;
+
+            this._trees.push(new Tree(x, height, color));
+        }
+    }
+
+    drawSky() {
+        var bg = c.createLinearGradient(0, canvas.height, 0, 0);
+
+        bg.addColorStop(0, 'white');
+        bg.addColorStop(1, '#39a8e1');
+
+        c.fillStyle = bg;
+        c.fillRect(0, 0, canvas.width, canvas.height);
+    }
+
+
+    drawTrees() {
+        for(let i = 0; i < this._trees.length; i++) {
+            this._trees[i].draw();
+        }
+    }
+
+    draw() {
+        this.drawSky();
+        this.drawTrees();
+    }
+
+    resize() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        centerX = canvas.width / 2;
+        centerY = canvas.height / 2;
     }
 }
-
-function drawSky() {
-    var bg = c.createLinearGradient(0, canvas.height, 0, 0);
-
-    bg.addColorStop(0, 'white');
-    bg.addColorStop(1, '#39a8e1');
-
-    c.fillStyle = bg;
-    c.fillRect(0, 0, canvas.width, canvas.height);
-}
-
-function drawTree(x, height, color) {
-    var treeHeight = height || 400;
-    var startY = canvas.height;
-    var treeWidth = treeHeight / 4;
-    var numTreeRungs = 3;
-
-    var rungWidth = treeWidth;
-    var rungBaseY = startY;
-    var progress;
-
-    c.fillStyle = color || TREE_COLOR;
-
-    // draw first rung
-    for (var i = 0; i < numTreeRungs; i++) {
-        progress = i / numTreeRungs;
-
-        rungBaseY = -treeHeight * Math.pow(progress, 1.2) + startY;
-        rungWidth = -treeWidth * Math.pow(progress, 3) + treeWidth;
-
-        c.beginPath();
-        c.moveTo(x - rungWidth, rungBaseY);
-        c.lineTo(x, canvas.height - treeHeight);
-        c.lineTo(x + rungWidth, rungBaseY);
-        c.fill();
-    }
-}
-
-function drawTrees() {
-    var i;
-    var pos;
-    var color;
-    var height;
-    var progress;
-    var numTrees = 40;
-    var maxTreeHeight = 400;
-    var minTreeHeight = 220;
-    var treeHeightDelta = maxTreeHeight - minTreeHeight;
-
-    var h = 0;
-    var s = 0;
-    var l = 0;
-
-    var maxH = 150;
-    var minH = 100;
-    var deltaH = maxH - minH;
-
-    var maxS = 100;
-    var minS = 70;
-    var deltaS = maxS - minS;
-
-    var maxL = 40;
-    var minL = 25;
-    var deltaL = maxL - minL;
-
-    for (i = 0; i < numTrees; i++) {
-        // drawSky(1 / numTrees); // create atmosphere effect;
-
-        progress = i / numTrees;
-
-        h = Math.floor(deltaH * Math.random() + minH);
-        s = Math.floor(deltaS * Math.random() + minS);
-        l = Math.floor(deltaL * Math.random() + minL);
-
-        l = minL + Math.random() * progress * deltaL;
-
-        color = 'hsl(' + h + ', ' + s + '%, ' + l + '%)';
-
-        pos = canvas.width * Math.random();
-        height = treeHeightDelta * Math.random() * progress + minTreeHeight;
-        drawTree(pos, height, color);
-    }
-}
-
-function draw() {
-    drawSky();
-    drawTrees();
-}
-
-function resize() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    centerX = canvas.width / 2;
-    centerY = canvas.height / 2;
-
-    draw();
-}
-
-function init() {
-}
-
-init();
